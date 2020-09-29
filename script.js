a0873100c252769cee55c68a7453ce52077e007f
Punith Gowda T <poppunith2001@gmail.com>
2020-09-29 22:18:23 +0530

    Show more button added

--- script.js
+++ script.js
@@ -13,4 +13,18 @@ const setProperty = (duration) => {
   
   setInterval(changeAnimationTime, 100);
   
+  function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
   
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Show More"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Show Less"; 
+      moreText.style.display = "inline";
+    }
+  }
