ef50904f4185f49713dad486d1120215eb5c9457
Punith Gowda T <poppunith2001@gmail.com>
2020-09-29 23:49:26 +0530

    show more button fixed

--- script.js
+++ script.js
@@ -14,17 +14,14 @@ const setProperty = (duration) => {
   setInterval(changeAnimationTime, 100);
   
   function myFunction() {
-    var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
     var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
+
+    if (more.style.display === "none") {
+      btnText.innerHTML = "Show Less"; 
+      moreText.style.display = "block";
+    } else {  
       btnText.innerHTML = "Show More"; 
       moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Show Less"; 
-      moreText.style.display = "inline";
     }
   }
